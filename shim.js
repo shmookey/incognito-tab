6c46afc8200f5081e08087989e1cef436256638c
Luke Williams <shmookey@shmookey.net>
2019-08-06 00:24:11 +0800

    Add icons, use FakeStorage instead of empty object

--- shim.js
+++ shim.js
@@ -1,19 +1,30 @@
-let FakeStorage = () =>
- ({ _data:       {}
-  , key:         function(n)   { return Object.keys(this._data)[n] }
-  , getItem:     function(k)   { return this._data[k] }
-  , setItem:     function(k,v) { this._data[k] = v }
-  , removeItem:  function(k)   { delete this._data[k] }
-  , clear:       function()    { this._data = {} }
-  , get length()               { return Object.keys(this._data).length }
-  })
+mockStorage = `
+  FakeStorage = () =>
+   ({ _data:       {}
+    , key:         function(n)   { return Object.keys(this._data)[n] }
+    , getItem:     function(k)   { return this._data[k] }
+    , setItem:     function(k,v) { this._data[k] = v }
+    , removeItem:  function(k)   { delete this._data[k] }
+    , clear:       function()    { this._data = {} }
+    , get length()               { return Object.keys(this._data).length }
+    })
+  
+  Object.defineProperty(window, "localStorage", 
+    { value:        FakeStorage()
+    , configurable: false
+    , writable:     false }
+  )
+  Object.defineProperty(window, "sessionStorage", 
+    { value:        FakeStorage()
+    , configurable: false
+    , writable:     false }
+  )
+`
 
-function replaceStorage() {
+window.addEventListener('DOMContentLoaded', () => {
   let elem = document.createElement("script")
   elem.type = "text/javascript"
-  elem.innerHTML = 'Object.defineProperty(window, "localStorage", { value: {}, configurable: false, writable: false })'
+  elem.innerHTML = mockStorage
   document.getElementsByTagName("head")[0].appendChild(elem)
-}
-
-window.addEventListener('DOMContentLoaded', () => replaceStorage())
+})
 
